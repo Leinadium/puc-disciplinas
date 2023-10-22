838fb8dc1afe60a963f5c90adfa8a214fd55f82c
Leinadium <daniel.sch.guima@gmail.com>
2023-10-22 20:20:52 -0300

    front: inicio do popup

---
+++ front/src/components/common/Popup.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import { createEventDispatcher } from "svelte";
+
+    const dispatch = createEventDispatcher();
+    const close = () => { dispatch('close');}
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div id="popup" on:click={close}>
+    <slot></slot>
+</div>
+
+<style>
+    #popup {
+        position: fixed;
+        top: 0;
+        left: 0;
+        z-index: 1000;
+        width: 100vw;
+        height: 100vh;
+        background-color: rgba(0, 0, 0, 0.5);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
---
+++ front/src/components/grade/TurmaSelecao.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { onMount, createEventDispatcher } from "svelte";
+	import Popup from "../common/Popup.svelte";
+
+    export let codigoDisciplina: string;
+
+    
+    onMount(() => {});
+</script>
+
+<Popup on:close>
+    <div id="turma-selecao">
+        <span>codigo</span>
+        <span>nome</span>
+        <span>ementa</span>
+        <span>creditos</span>
+        <span>pre-requisitos</span>
+        <span>avaliacao</span>
+        <span>grau médio</span>
+        <span>quantidade de alunos</span>
+
+        <div id="turmas">
+            <div class="turma">
+                <span>codigo</span>         <!-- turmas    -->
+                <span>professor</span>      <!-- turmas    -->
+                <span>shf</span>            <!-- turmas    -->
+                <span>horario</span>        <!-- horarios  -->
+                <span>destino</span>        <!-- alocacoes -->
+                <span>qtdVagas</span>       <!-- alocacoes -->
+            </div>
+        </div>
+
+
+    </div>
+</Popup>
