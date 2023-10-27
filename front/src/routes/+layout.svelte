42f4a69659bb0f23efb1985bc27fe8cde9de8657
Leinadium <daniel.sch.guima@gmail.com>
2023-10-27 12:35:08 -0300

    front: adicionando cores css

--- front/src/routes/+layout.svelte
+++ front/src/routes/+layout.svelte
@@ -16,4 +16,18 @@
         width: 100%;
         height: 100%;
     }
+
+    :global(html) {
+        --color-main-1: #04364A;
+        --color-main-2: #176B87;
+        --color-main-3: #64CCC5;
+        --color-main-4: #DAFFFB;
+        --color-tria-1: #8C8111;
+        --color-tria-2: #8C1140;
+        --color-tria-3: #11768C;
+        --color-tria-4: #193237;
+        --color-tria-5: #FFF69E;
+
+        --border-radius: 15px;
+    }
 </style>
