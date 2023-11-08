a72fed46721468ceb102f3f60ca8fb6d08bbe681
Leinadium <daniel.sch.guima@gmail.com>
2023-11-08 07:34:08 -0300

    data: revertendo restricao do curriculo

--- data/carga/curriculo-cie-23.sql
+++ data/carga/curriculo-cie-23.sql
@@ -1,68 +1,123 @@
 -- tabela curriculos
-DELETE FROM curriculos WHERE cod_curriculo = 'CCPBCC20230';
+DELETE FROM curriculos WHERE cod_curriculo = 'CCPBCO20230';
 
 INSERT INTO curriculos (cod_curriculo, nome_curriculo)
-VALUES ('CCPBCC20230', 'Ciência da Computação (Bacharelado) - Currículo 23.0');
+VALUES ('CCPBCO20230', 'Ciência da Computação (Bacharelado) - Currículo 23.0');
 
 -- tabela de semestres
-DELETE FROM semestres WHERE cod_curriculo = 'CCPBCC20230';
+DELETE FROM semestres WHERE cod_curriculo = 'CCPBCO20230';
+
+-- INSERT INTO semestres (cod_curriculo, cod_disciplina, semestre)
+-- SELECT * FROM (VALUES
+--     ('CCPBCO20230', 'CRE1200', 1),
+--     ('CCPBCO20230', 'CTC4002', 1),
+--     ('CCPBCO20230', 'INF1009', 1),
+--     ('CCPBCO20230', 'FIS1034', 1),
+--     ('CCPBCO20230', 'INF1012', 1),
+--     ('CCPBCO20230', 'INF1034', 1),
+--     ('CCPBCO20230', 'LET1920', 1),
+--
+--     ('CCPBCO20230', 'INF1037', 2),
+--     ('CCPBCO20230', 'INF1039', 2),
+--     ('CCPBCO20230', 'INF1403', 2),
+--     ('CCPBCO20230', 'MAT4161', 2),
+--     ('CCPBCO20230', 'MAT4200', 2),
+--
+--     ('CCPBCO20230', 'CRE0712', 3),
+--     ('CCPBCO20230', 'INF1010', 3),
+--     ('CCPBCO20230', 'INF1018', 3),
+--     ('CCPBCO20230', 'INF1040', 3),
+--     ('CCPBCO20230', 'INF1383', 3),
+--     ('CCPBCO20230', 'MAT4162', 3),
+--
+--     ('CCPBCO20230', 'INF0381', 4),
+--     ('CCPBCO20230', 'INF1027', 4),
+--     ('CCPBCO20230', 'INF1041', 4),
+--     ('CCPBCO20230', 'INF1316', 4),
+--     ('CCPBCO20230', 'INF1636', 4),
+--     ('CCPBCO20230', 'INF1320', 4),
+--     ('CCPBCO20230', 'MAT4202', 4),
+--
+--     ('CCPBCO20230', 'CRE1241', 5),
+--     ('CCPBCO20230', 'INF0377', 5),
+--     ('CCPBCO20230', 'INF1022', 5),
+--     ('CCPBCO20230', 'INF1029', 5),
+--     ('CCPBCO20230', 'INF1631', 5),
+--     ('CCPBCO20230', 'INF1640', 5),
+--
+--     ('CCPBCO20230', 'CRE1275', 6),
+--     ('CCPBCO20230', 'INF0305', 6),
+--     ('CCPBCO20230', 'INF0306', 6),
+--     ('CCPBCO20230', 'INF1014', 6),
+--     ('CCPBCO20230', 'INF1028', 6),
+--     ('CCPBCO20230', 'INF1721', 6),
+--     ('CCPBCO20230', 'INF1920', 6),
+--
+--     ('CCPBCO20230', 'INF0303', 7),
+--     ('CCPBCO20230', 'INF0308', 7),
+--     ('CCPBCO20230', 'INF1416', 7),
+--     ('CCPBCO20230', 'INF1950', 7),
+--
+--     ('CCPBCO20230', 'FIL0300', 8),
+--     ('CCPBCO20230', 'INF1951', 8)
+-- ) AS i (cod_curriculo, cod_disciplina, semestre)
+-- WHERE EXISTS (
+--     SELECT cod_disciplina
+--     FROM disciplinas
+--     WHERE cod_disciplina = i.cod_disciplina
+-- );
 
 INSERT INTO semestres (cod_curriculo, cod_disciplina, semestre)
-SELECT * FROM (VALUES
-    ('CCPBCC20230', 'CRE1200', 1),
-    ('CCPBCC20230', 'CTC4002', 1),
-    ('CCPBCC20230', 'INF1009', 1),
-    ('CCPBCC20230', 'FIS1034', 1),
-    ('CCPBCC20230', 'INF1012', 1),
-    ('CCPBCC20230', 'INF1034', 1),
-    ('CCPBCC20230', 'LET1920', 1),
+VALUES
+    ('CCPBCO20230', 'CRE1200', 1),
+    ('CCPBCO20230', 'CTC4002', 1),
+    ('CCPBCO20230', 'INF1009', 1),
+    ('CCPBCO20230', 'FIS1034', 1),
+    ('CCPBCO20230', 'INF1012', 1),
+    ('CCPBCO20230', 'INF1034', 1),
+    ('CCPBCO20230', 'LET1920', 1),
 
-    ('CCPBCC20230', 'INF1037', 2),
-    ('CCPBCC20230', 'INF1039', 2),
-    ('CCPBCC20230', 'INF1403', 2),
-    ('CCPBCC20230', 'MAT4161', 2),
-    ('CCPBCC20230', 'MAT4200', 2),
+    ('CCPBCO20230', 'INF1037', 2),
+    ('CCPBCO20230', 'INF1039', 2),
+    ('CCPBCO20230', 'INF1403', 2),
+    ('CCPBCO20230', 'MAT4161', 2),
+    ('CCPBCO20230', 'MAT4200', 2),
 
-    ('CCPBCC20230', 'CRE0712', 3),
-    ('CCPBCC20230', 'INF1010', 3),
-    ('CCPBCC20230', 'INF1018', 3),
-    ('CCPBCC20230', 'INF1040', 3),
-    ('CCPBCC20230', 'INF1383', 3),
-    ('CCPBCC20230', 'MAT4162', 3),
+    ('CCPBCO20230', 'CRE0712', 3),
+    ('CCPBCO20230', 'INF1010', 3),
+    ('CCPBCO20230', 'INF1018', 3),
+    ('CCPBCO20230', 'INF1040', 3),
+    ('CCPBCO20230', 'INF1383', 3),
+    ('CCPBCO20230', 'MAT4162', 3),
 
-    ('CCPBCC20230', 'INF0381', 4),
-    ('CCPBCC20230', 'INF1027', 4),
-    ('CCPBCC20230', 'INF1041', 4),
-    ('CCPBCC20230', 'INF1316', 4),
-    ('CCPBCC20230', 'INF1636', 4),
-    ('CCPBCC20230', 'INF1320', 4),
-    ('CCPBCC20230', 'MAT4202', 4),
+    ('CCPBCO20230', 'INF0381', 4),
+    ('CCPBCO20230', 'INF1027', 4),
+    ('CCPBCO20230', 'INF1041', 4),
+    ('CCPBCO20230', 'INF1316', 4),
+    ('CCPBCO20230', 'INF1636', 4),
+    ('CCPBCO20230', 'INF1320', 4),
+    ('CCPBCO20230', 'MAT4202', 4),
 
-    ('CCPBCC20230', 'CRE1241', 5),
-    ('CCPBCC20230', 'INF0377', 5),
-    ('CCPBCC20230', 'INF1022', 5),
-    ('CCPBCC20230', 'INF1029', 5),
-    ('CCPBCC20230', 'INF1631', 5),
-    ('CCPBCC20230', 'INF1640', 5),
+    ('CCPBCO20230', 'CRE1241', 5),
+    ('CCPBCO20230', 'INF0377', 5),
+    ('CCPBCO20230', 'INF1022', 5),
+    ('CCPBCO20230', 'INF1029', 5),
+    ('CCPBCO20230', 'INF1631', 5),
+    ('CCPBCO20230', 'INF1640', 5),
 
-    ('CCPBCC20230', 'CRE1275', 6),
-    ('CCPBCC20230', 'INF0305', 6),
-    ('CCPBCC20230', 'INF0306', 6),
-    ('CCPBCC20230', 'INF1014', 6),
-    ('CCPBCC20230', 'INF1028', 6),
-    ('CCPBCC20230', 'INF1721', 6),
-    ('CCPBCC20230', 'INF1920', 6),
+    ('CCPBCO20230', 'CRE1275', 6),
+    ('CCPBCO20230', 'INF0305', 6),
+    ('CCPBCO20230', 'INF0306', 6),
+    ('CCPBCO20230', 'INF1014', 6),
+    ('CCPBCO20230', 'INF1028', 6),
+    ('CCPBCO20230', 'INF1721', 6),
+    ('CCPBCO20230', 'INF1920', 6),
 
-    ('CCPBCC20230', 'INF0303', 7),
-    ('CCPBCC20230', 'INF0308', 7),
-    ('CCPBCC20230', 'INF1416', 7),
-    ('CCPBCC20230', 'INF1950', 7),
+    ('CCPBCO20230', 'INF0303', 7),
+    ('CCPBCO20230', 'INF0308', 7),
+    ('CCPBCO20230', 'INF1416', 7),
+    ('CCPBCO20230', 'INF1950', 7),
 
-    ('CCPBCC20230', 'FIL0300', 8),
-    ('CCPBCC20230', 'INF1951', 8)
-) AS i (cod_curriculo, cod_disciplina, semestre)
-WHERE EXISTS (
-    SELECT cod_disciplina
-    FROM disciplinas
-    WHERE cod_disciplina = i.cod_disciplina
-);
+    ('CCPBCO20230', 'FIL0300', 8),
+    ('CCPBCO20230', 'INF1951', 8)
+;
--- data/carga/curriculo-eng-23.sql
+++ data/carga/curriculo-eng-23.sql
@@ -7,8 +7,88 @@ VALUES ('CEGBCO20231', 'Eng. de Computação (Bacharelado) - Currículo 23.1');
 -- tabela de semestres
 DELETE FROM semestres WHERE cod_curriculo = 'CEGBCO20231';
 
+-- INSERT INTO semestres (cod_curriculo, cod_disciplina, semestre)
+-- SELECT * FROM (VALUES
+--     ('CEGBCO20231', 'ENG4010', 1),
+--     ('CEGBCO20231', 'EMP1310', 1),
+--     ('CEGBCO20231', 'ENG4025', 1),
+--     ('CEGBCO20231', 'CTC4001', 1),
+--     ('CEGBCO20231', 'CTC4002', 1),
+--     ('CEGBCO20231', 'CTC4003', 1),
+--     ('CEGBCO20231', 'CRE1200', 1),
+--
+--     ('CEGBCO20231', 'ENG4021', 2),
+--     ('CEGBCO20231', 'EMP1320', 2),
+--     ('CEGBCO20231', 'INF1037', 2),
+--     ('CEGBCO20231', 'MAT4161', 3),
+--     ('CEGBCO20231', 'FIS4001', 2),
+--     ('CEGBCO20231', 'MAT4200', 2),
+--     ('CEGBCO20231', 'INF1012', 2),
+--     ('CEGBCO20231', 'INF1009', 2),
+--
+--     ('CEGBCO20231', 'ENG4033', 3),
+--     ('CEGBCO20231', 'ENG4431', 3),
+--     ('CEGBCO20231', 'ENG4420', 3),
+--     ('CEGBCO20231', 'MAT4162', 3),
+--     ('CEGBCO20231', 'FIS4002', 3),
+--     ('CEGBCO20231', 'MAT4202', 3),
+--     ('CEGBCO20231', 'INF1383', 3),
+--     ('CEGBCO20231', 'ENG4007', 3),
+--
+--     ('CEGBCO20231', 'ENG4040', 4),
+--     ('CEGBCO20231', 'ENG4502', 4),
+--     ('CEGBCO20231', 'ENG4501', 4),
+--     ('CEGBCO20231', 'ENG4402', 4),
+--     ('CEGBCO20231', 'MAT4174', 4),
+--     ('CEGBCO20231', 'INF1010', 4),
+--     ('CEGBCO20231', 'INF1018', 4),
+--     ('CEGBCO20231', 'ENG4011', 4),
+--
+--     ('CEGBCO20231', 'ENG4051', 5),
+--     ('CEGBCO20231', 'ENG4013', 5),
+--     ('CEGBCO20231', 'ENG4421', 5),
+--     ('CEGBCO20231', 'MAT1320', 5),
+--     ('CEGBCO20231', 'FIS4003', 5),
+--     ('CEGBCO20231', 'INF1301', 5),
+--
+--     ('CEGBCO20231', 'ENG4061', 6),
+--     ('CEGBCO20231', 'ENG4448', 6),
+--     ('CEGBCO20231', 'INF1022', 6),
+--     ('CEGBCO20231', 'INF1631', 6),
+--     ('CEGBCO20231', 'INF1636', 6),
+--     ('CEGBCO20231', 'INF1316', 6),
+--     ('CEGBCO20231', 'CRE0712', 6),
+--
+--     ('CEGBCO20231', 'ENG4405', 7),
+--     ('CEGBCO20231', 'INF1721', 7),
+--     ('CEGBCO20231', 'INF1041', 7),
+--     ('CEGBCO20231', 'INF1029', 7),
+--     ('CEGBCO20231', 'INF1640', 7),
+--     ('CEGBCO20231', 'FIL0300', 7),
+--     ('CEGBCO20231', 'CRE1241', 7),
+--
+--     ('CEGBCO20231', 'ENG4030', 8),
+--     ('CEGBCO20231', 'EMP1330', 8),
+--     ('CEGBCO20231', 'ENG4500', 8),
+--     ('CEGBCO20231', 'INF0312', 8),
+--     ('CEGBCO20231', 'INF0303', 8),
+--     ('CEGBCO20231', 'INF0305', 8),
+--     ('CEGBCO20231', 'JUR0205', 8),
+--
+--     ('CEGBCO20231', 'ENG4134', 9),
+--     ('CEGBCO20231', 'CRE1275', 9),
+--
+--     ('CEGBCO20231', 'ENG4135', 10),
+--     ('CEGBCO20231', 'ENG4153', 10)
+-- ) AS i (cod_curriculo, cod_disciplina, semestre)
+-- WHERE EXISTS (
+--     SELECT cod_disciplina
+--     FROM disciplinas
+--     WHERE cod_disciplina = i.cod_disciplina
+-- );
+
 INSERT INTO semestres (cod_curriculo, cod_disciplina, semestre)
-SELECT * FROM (VALUES
+VALUES
     ('CEGBCO20231', 'ENG4010', 1),
     ('CEGBCO20231', 'EMP1310', 1),
     ('CEGBCO20231', 'ENG4025', 1),
@@ -80,9 +160,4 @@ SELECT * FROM (VALUES
 
     ('CEGBCO20231', 'ENG4135', 10),
     ('CEGBCO20231', 'ENG4153', 10)
-) AS i (cod_curriculo, cod_disciplina, semestre)
-WHERE EXISTS (
-    SELECT cod_disciplina
-    FROM disciplinas
-    WHERE cod_disciplina = i.cod_disciplina
-);
+;
