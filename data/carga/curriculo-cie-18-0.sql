90f53272a3eea02880af144e5d9c24ee0b776b08
Leinadium <daniel.sch.guima@gmail.com>
2023-11-08 07:37:07 -0300

    data: refazendo substituicao BCC

--- data/carga/curriculo-cie-18-0.sql
+++ data/carga/curriculo-cie-18-0.sql
@@ -1,57 +1,57 @@
 -- tabela curriculos
-DELETE FROM curriculos WHERE cod_curriculo = 'CCPBCO20180';
+DELETE FROM curriculos WHERE cod_curriculo = 'CCPBCC20180';
 
 INSERT INTO curriculos (cod_curriculo, nome_curriculo)
-VALUES ('CCPBCO20180', 'Ciência da Computação (Bacharelado) - Currículo 18.0');
+VALUES ('CCPBCC20180', 'Ciência da Computação (Bacharelado) - Currículo 18.0');
 
 -- tabela de semestres
-DELETE FROM semestres WHERE cod_curriculo = 'CCPBCO20180';
+DELETE FROM semestres WHERE cod_curriculo = 'CCPBCC20180';
 
 -- INSERT INTO semestres (cod_curriculo, cod_disciplina, semestre)
 -- SELECT * FROM (VALUES
---     ('CCPBCO20180', 'INF1025', 1),
---     ('CCPBCO20180', 'INF1012', 1),
---     ('CCPBCO20180', 'INF1009', 1),
---     ('CCPBCO20180', 'INF1031', 1),
---     ('CCPBCO20180', 'MAT1161', 1),
---     ('CCPBCO20180', 'MAT1320', 1),
+--     ('CCPBCC20180', 'INF1025', 1),
+--     ('CCPBCC20180', 'INF1012', 1),
+--     ('CCPBCC20180', 'INF1009', 1),
+--     ('CCPBCC20180', 'INF1031', 1),
+--     ('CCPBCC20180', 'MAT1161', 1),
+--     ('CCPBCC20180', 'MAT1320', 1),
 --
---     ('CCPBCO20180', 'CRE1100', 2),
---     ('CCPBCO20180', 'INF0380', 2),
---     ('CCPBCO20180', 'INF1403', 2),
---     ('CCPBCO20180', 'INF1007', 2),
---     ('CCPBCO20180', 'MAT0211', 2),
+--     ('CCPBCC20180', 'CRE1100', 2),
+--     ('CCPBCC20180', 'INF0380', 2),
+--     ('CCPBCC20180', 'INF1403', 2),
+--     ('CCPBCC20180', 'INF1007', 2),
+--     ('CCPBCC20180', 'MAT0211', 2),
 --
---     ('CCPBCO20180', 'INF1383', 3),
---     ('CCPBCO20180', 'INF1631', 3),
---     ('CCPBCO20180', 'INF1010', 3),
---     ('CCPBCO20180', 'INF1301', 3),
---     ('CCPBCO20180', 'INF1018', 3),
---     ('CCPBCO20180', 'MAT1162', 3),
+--     ('CCPBCC20180', 'INF1383', 3),
+--     ('CCPBCC20180', 'INF1631', 3),
+--     ('CCPBCC20180', 'INF1010', 3),
+--     ('CCPBCC20180', 'INF1301', 3),
+--     ('CCPBCC20180', 'INF1018', 3),
+--     ('CCPBCC20180', 'MAT1162', 3),
 --
---     ('CCPBCO20180', 'CRE0710', 4),
---     ('CCPBCO20180', 'INF1027', 4),
---     ('CCPBCO20180', 'INF1029', 4),
---     ('CCPBCO20180', 'INF1316', 4),
---     ('CCPBCO20180', 'INF1629', 4),
---     ('CCPBCO20180', 'INF1636', 4),
+--     ('CCPBCC20180', 'CRE0710', 4),
+--     ('CCPBCC20180', 'INF1027', 4),
+--     ('CCPBCC20180', 'INF1029', 4),
+--     ('CCPBCC20180', 'INF1316', 4),
+--     ('CCPBCC20180', 'INF1629', 4),
+--     ('CCPBCC20180', 'INF1636', 4),
 --
---     ('CCPBCO20180', 'CRE1141', 5),
---     ('CCPBCO20180', 'INF0377', 5),
---     ('CCPBCO20180', 'INF1022', 5),
---     ('CCPBCO20180', 'INF1028', 5),
---     ('CCPBCO20180', 'INF1317', 5),
---     ('CCPBCO20180', 'INF1721', 5),
+--     ('CCPBCC20180', 'CRE1141', 5),
+--     ('CCPBCC20180', 'INF0377', 5),
+--     ('CCPBCC20180', 'INF1022', 5),
+--     ('CCPBCC20180', 'INF1028', 5),
+--     ('CCPBCC20180', 'INF1317', 5),
+--     ('CCPBCC20180', 'INF1721', 5),
 --
---     ('CCPBCO20180', 'INF1014', 6),
---     ('CCPBCO20180', 'INF1033', 6),
+--     ('CCPBCC20180', 'INF1014', 6),
+--     ('CCPBCC20180', 'INF1033', 6),
 --
---     ('CCPBCO20180', 'CRE1175', 7),
---     ('CCPBCO20180', 'FIL0300', 7),
---     ('CCPBCO20180', 'INF1950', 7),
+--     ('CCPBCC20180', 'CRE1175', 7),
+--     ('CCPBCC20180', 'FIL0300', 7),
+--     ('CCPBCC20180', 'INF1950', 7),
 --
---     ('CCPBCO20180', 'INF0381', 8),
---     ('CCPBCO20180', 'INF1951', 8)
+--     ('CCPBCC20180', 'INF0381', 8),
+--     ('CCPBCC20180', 'INF1951', 8)
 -- ) AS i (cod_curriculo, cod_disciplina, semestre)
 -- WHERE EXISTS (
 --     SELECT cod_disciplina
@@ -61,47 +61,47 @@ DELETE FROM semestres WHERE cod_curriculo = 'CCPBCO20180';
 
 INSERT INTO semestres (cod_curriculo, cod_disciplina, semestre)
 VALUES
-    ('CCPBCO20180', 'INF1025', 1),
-    ('CCPBCO20180', 'INF1012', 1),
-    ('CCPBCO20180', 'INF1009', 1),
-    ('CCPBCO20180', 'INF1031', 1),
-    ('CCPBCO20180', 'MAT1161', 1),
-    ('CCPBCO20180', 'MAT1320', 1),
+    ('CCPBCC20180', 'INF1025', 1),
+    ('CCPBCC20180', 'INF1012', 1),
+    ('CCPBCC20180', 'INF1009', 1),
+    ('CCPBCC20180', 'INF1031', 1),
+    ('CCPBCC20180', 'MAT1161', 1),
+    ('CCPBCC20180', 'MAT1320', 1),
 
-    ('CCPBCO20180', 'CRE1100', 2),
-    ('CCPBCO20180', 'INF0380', 2),
-    ('CCPBCO20180', 'INF1403', 2),
-    ('CCPBCO20180', 'INF1007', 2),
-    ('CCPBCO20180', 'MAT0211', 2),
+    ('CCPBCC20180', 'CRE1100', 2),
+    ('CCPBCC20180', 'INF0380', 2),
+    ('CCPBCC20180', 'INF1403', 2),
+    ('CCPBCC20180', 'INF1007', 2),
+    ('CCPBCC20180', 'MAT0211', 2),
 
-    ('CCPBCO20180', 'INF1383', 3),
-    ('CCPBCO20180', 'INF1631', 3),
-    ('CCPBCO20180', 'INF1010', 3),
-    ('CCPBCO20180', 'INF1301', 3),
-    ('CCPBCO20180', 'INF1018', 3),
-    ('CCPBCO20180', 'MAT1162', 3),
+    ('CCPBCC20180', 'INF1383', 3),
+    ('CCPBCC20180', 'INF1631', 3),
+    ('CCPBCC20180', 'INF1010', 3),
+    ('CCPBCC20180', 'INF1301', 3),
+    ('CCPBCC20180', 'INF1018', 3),
+    ('CCPBCC20180', 'MAT1162', 3),
 
-    ('CCPBCO20180', 'CRE0710', 4),
-    ('CCPBCO20180', 'INF1027', 4),
-    ('CCPBCO20180', 'INF1029', 4),
-    ('CCPBCO20180', 'INF1316', 4),
-    ('CCPBCO20180', 'INF1629', 4),
-    ('CCPBCO20180', 'INF1636', 4),
+    ('CCPBCC20180', 'CRE0710', 4),
+    ('CCPBCC20180', 'INF1027', 4),
+    ('CCPBCC20180', 'INF1029', 4),
+    ('CCPBCC20180', 'INF1316', 4),
+    ('CCPBCC20180', 'INF1629', 4),
+    ('CCPBCC20180', 'INF1636', 4),
 
-    ('CCPBCO20180', 'CRE1141', 5),
-    ('CCPBCO20180', 'INF0377', 5),
-    ('CCPBCO20180', 'INF1022', 5),
-    ('CCPBCO20180', 'INF1028', 5),
-    ('CCPBCO20180', 'INF1317', 5),
-    ('CCPBCO20180', 'INF1721', 5),
+    ('CCPBCC20180', 'CRE1141', 5),
+    ('CCPBCC20180', 'INF0377', 5),
+    ('CCPBCC20180', 'INF1022', 5),
+    ('CCPBCC20180', 'INF1028', 5),
+    ('CCPBCC20180', 'INF1317', 5),
+    ('CCPBCC20180', 'INF1721', 5),
 
-    ('CCPBCO20180', 'INF1014', 6),
-    ('CCPBCO20180', 'INF1033', 6),
+    ('CCPBCC20180', 'INF1014', 6),
+    ('CCPBCC20180', 'INF1033', 6),
 
-    ('CCPBCO20180', 'CRE1175', 7),
-    ('CCPBCO20180', 'FIL0300', 7),
-    ('CCPBCO20180', 'INF1950', 7),
+    ('CCPBCC20180', 'CRE1175', 7),
+    ('CCPBCC20180', 'FIL0300', 7),
+    ('CCPBCC20180', 'INF1950', 7),
 
-    ('CCPBCO20180', 'INF0381', 8),
-    ('CCPBCO20180', 'INF1951', 8)
+    ('CCPBCC20180', 'INF0381', 8),
+    ('CCPBCC20180', 'INF1951', 8)
 ;
--- data/carga/curriculo-cie-18-1.sql
+++ data/carga/curriculo-cie-18-1.sql
@@ -1,58 +1,58 @@
 -- tabela curriculos
-DELETE FROM curriculos WHERE cod_curriculo = 'CCPBCO20181';
+DELETE FROM curriculos WHERE cod_curriculo = 'CCPBCC20181';
 
 INSERT INTO curriculos (cod_curriculo, nome_curriculo)
-VALUES ('CCPBCO20181', 'Ciência da Computação (Bacharelado) - Currículo 18.1');
+VALUES ('CCPBCC20181', 'Ciência da Computação (Bacharelado) - Currículo 18.1');
 
 -- tabela de semestres
-DELETE FROM semestres WHERE cod_curriculo = 'CCPBCO20181';
+DELETE FROM semestres WHERE cod_curriculo = 'CCPBCC20181';
 
 -- INSERT INTO semestres (cod_curriculo, cod_disciplina, semestre)
 -- SELECT * FROM (VALUES
---     ('CCPBCO20181', 'INF1025', 1),
---     ('CCPBCO20181', 'INF1012', 1),
---     ('CCPBCO20181', 'INF1009', 1),
---     ('CCPBCO20181', 'INF1031', 1),
---     ('CCPBCO20181', 'MAT1157', 1),
---     ('CCPBCO20181', 'MAT1320', 1),
+--     ('CCPBCC20181', 'INF1025', 1),
+--     ('CCPBCC20181', 'INF1012', 1),
+--     ('CCPBCC20181', 'INF1009', 1),
+--     ('CCPBCC20181', 'INF1031', 1),
+--     ('CCPBCC20181', 'MAT1157', 1),
+--     ('CCPBCC20181', 'MAT1320', 1),
 --
---     ('CCPBCO20181', 'CRE1100', 2),
---     ('CCPBCO20181', 'INF0380', 2),
---     ('CCPBCO20181', 'INF1403', 2),
---     ('CCPBCO20181', 'INF1007', 2),
---     ('CCPBCO20181', 'MAT1158', 2),
---     ('CCPBCO20181', 'MAT1250', 2),
+--     ('CCPBCC20181', 'CRE1100', 2),
+--     ('CCPBCC20181', 'INF0380', 2),
+--     ('CCPBCC20181', 'INF1403', 2),
+--     ('CCPBCC20181', 'INF1007', 2),
+--     ('CCPBCC20181', 'MAT1158', 2),
+--     ('CCPBCC20181', 'MAT1250', 2),
 --
---     ('CCPBCO20181', 'INF1383', 3),
---     ('CCPBCO20181', 'INF1631', 3),
---     ('CCPBCO20181', 'INF1010', 3),
---     ('CCPBCO20181', 'INF1301', 3),
---     ('CCPBCO20181', 'INF1018', 3),
---     ('CCPBCO20181', 'MAT1162', 3),
+--     ('CCPBCC20181', 'INF1383', 3),
+--     ('CCPBCC20181', 'INF1631', 3),
+--     ('CCPBCC20181', 'INF1010', 3),
+--     ('CCPBCC20181', 'INF1301', 3),
+--     ('CCPBCC20181', 'INF1018', 3),
+--     ('CCPBCC20181', 'MAT1162', 3),
 --
---     ('CCPBCO20181', 'CRE0710', 4),
---     ('CCPBCO20181', 'INF1027', 4),
---     ('CCPBCO20181', 'INF1029', 4),
---     ('CCPBCO20181', 'INF1316', 4),
---     ('CCPBCO20181', 'INF1629', 4),
---     ('CCPBCO20181', 'INF1636', 4),
+--     ('CCPBCC20181', 'CRE0710', 4),
+--     ('CCPBCC20181', 'INF1027', 4),
+--     ('CCPBCC20181', 'INF1029', 4),
+--     ('CCPBCC20181', 'INF1316', 4),
+--     ('CCPBCC20181', 'INF1629', 4),
+--     ('CCPBCC20181', 'INF1636', 4),
 --
---     ('CCPBCO20181', 'CRE1141', 5),
---     ('CCPBCO20181', 'INF0377', 5),
---     ('CCPBCO20181', 'INF1022', 5),
---     ('CCPBCO20181', 'INF1028', 5),
---     ('CCPBCO20181', 'INF1317', 5),
---     ('CCPBCO20181', 'INF1721', 5),
+--     ('CCPBCC20181', 'CRE1141', 5),
+--     ('CCPBCC20181', 'INF0377', 5),
+--     ('CCPBCC20181', 'INF1022', 5),
+--     ('CCPBCC20181', 'INF1028', 5),
+--     ('CCPBCC20181', 'INF1317', 5),
+--     ('CCPBCC20181', 'INF1721', 5),
 --
---     ('CCPBCO20181', 'INF1014', 6),
---     ('CCPBCO20181', 'INF1033', 6),
+--     ('CCPBCC20181', 'INF1014', 6),
+--     ('CCPBCC20181', 'INF1033', 6),
 --
---     ('CCPBCO20181', 'CRE1175', 7),
---     ('CCPBCO20181', 'FIL0300', 7),
---     ('CCPBCO20181', 'INF1950', 7),
+--     ('CCPBCC20181', 'CRE1175', 7),
+--     ('CCPBCC20181', 'FIL0300', 7),
+--     ('CCPBCC20181', 'INF1950', 7),
 --
---     ('CCPBCO20181', 'INF0381', 8),
---     ('CCPBCO20181', 'INF1951', 8)
+--     ('CCPBCC20181', 'INF0381', 8),
+--     ('CCPBCC20181', 'INF1951', 8)
 -- ) AS i (cod_curriculo, cod_disciplina, semestre)
 -- WHERE EXISTS (
 --     SELECT cod_disciplina
@@ -62,48 +62,48 @@ DELETE FROM semestres WHERE cod_curriculo = 'CCPBCO20181';
 
 INSERT INTO semestres (cod_curriculo, cod_disciplina, semestre)
 VALUES
-    ('CCPBCO20181', 'INF1025', 1),
-    ('CCPBCO20181', 'INF1012', 1),
-    ('CCPBCO20181', 'INF1009', 1),
-    ('CCPBCO20181', 'INF1031', 1),
-    ('CCPBCO20181', 'MAT1157', 1),
-    ('CCPBCO20181', 'MAT1320', 1),
+    ('CCPBCC20181', 'INF1025', 1),
+    ('CCPBCC20181', 'INF1012', 1),
+    ('CCPBCC20181', 'INF1009', 1),
+    ('CCPBCC20181', 'INF1031', 1),
+    ('CCPBCC20181', 'MAT1157', 1),
+    ('CCPBCC20181', 'MAT1320', 1),
 
-    ('CCPBCO20181', 'CRE1100', 2),
-    ('CCPBCO20181', 'INF0380', 2),
-    ('CCPBCO20181', 'INF1403', 2),
-    ('CCPBCO20181', 'INF1007', 2),
-    ('CCPBCO20181', 'MAT1158', 2),
-    ('CCPBCO20181', 'MAT1250', 2),
+    ('CCPBCC20181', 'CRE1100', 2),
+    ('CCPBCC20181', 'INF0380', 2),
+    ('CCPBCC20181', 'INF1403', 2),
+    ('CCPBCC20181', 'INF1007', 2),
+    ('CCPBCC20181', 'MAT1158', 2),
+    ('CCPBCC20181', 'MAT1250', 2),
 
-    ('CCPBCO20181', 'INF1383', 3),
-    ('CCPBCO20181', 'INF1631', 3),
-    ('CCPBCO20181', 'INF1010', 3),
-    ('CCPBCO20181', 'INF1301', 3),
-    ('CCPBCO20181', 'INF1018', 3),
-    ('CCPBCO20181', 'MAT1162', 3),
+    ('CCPBCC20181', 'INF1383', 3),
+    ('CCPBCC20181', 'INF1631', 3),
+    ('CCPBCC20181', 'INF1010', 3),
+    ('CCPBCC20181', 'INF1301', 3),
+    ('CCPBCC20181', 'INF1018', 3),
+    ('CCPBCC20181', 'MAT1162', 3),
 
-    ('CCPBCO20181', 'CRE0710', 4),
-    ('CCPBCO20181', 'INF1027', 4),
-    ('CCPBCO20181', 'INF1029', 4),
-    ('CCPBCO20181', 'INF1316', 4),
-    ('CCPBCO20181', 'INF1629', 4),
-    ('CCPBCO20181', 'INF1636', 4),
+    ('CCPBCC20181', 'CRE0710', 4),
+    ('CCPBCC20181', 'INF1027', 4),
+    ('CCPBCC20181', 'INF1029', 4),
+    ('CCPBCC20181', 'INF1316', 4),
+    ('CCPBCC20181', 'INF1629', 4),
+    ('CCPBCC20181', 'INF1636', 4),
 
-    ('CCPBCO20181', 'CRE1141', 5),
-    ('CCPBCO20181', 'INF0377', 5),
-    ('CCPBCO20181', 'INF1022', 5),
-    ('CCPBCO20181', 'INF1028', 5),
-    ('CCPBCO20181', 'INF1317', 5),
-    ('CCPBCO20181', 'INF1721', 5),
+    ('CCPBCC20181', 'CRE1141', 5),
+    ('CCPBCC20181', 'INF0377', 5),
+    ('CCPBCC20181', 'INF1022', 5),
+    ('CCPBCC20181', 'INF1028', 5),
+    ('CCPBCC20181', 'INF1317', 5),
+    ('CCPBCC20181', 'INF1721', 5),
 
-    ('CCPBCO20181', 'INF1014', 6),
-    ('CCPBCO20181', 'INF1033', 6),
+    ('CCPBCC20181', 'INF1014', 6),
+    ('CCPBCC20181', 'INF1033', 6),
 
-    ('CCPBCO20181', 'CRE1175', 7),
-    ('CCPBCO20181', 'FIL0300', 7),
-    ('CCPBCO20181', 'INF1950', 7),
+    ('CCPBCC20181', 'CRE1175', 7),
+    ('CCPBCC20181', 'FIL0300', 7),
+    ('CCPBCC20181', 'INF1950', 7),
 
-    ('CCPBCO20181', 'INF0381', 8),
-    ('CCPBCO20181', 'INF1951', 8)
+    ('CCPBCC20181', 'INF0381', 8),
+    ('CCPBCC20181', 'INF1951', 8)
 ;
