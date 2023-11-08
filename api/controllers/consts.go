951e6d768a57428c282b7201b1c3d8cd54e1f08b
Leinadium <daniel.sch.guima@gmail.com>
2023-11-08 11:59:35 -0300

    api: env para urls

--- api/controllers/consts.go
+++ api/controllers/consts.go
@@ -1,7 +1,19 @@
 package controllers
 
+import "os"
+
 // nome da chave da sessao do login
 const keyLoginSession = "usuario"
 const keyNomeSession = "nome"
-const urlMicroLogin = "http://localhost:5000"
-const urlMicroHistorico = "http://localhost:5001"
+
+func getEnvDefault(envName, envDefault string) string {
+	var v string
+	v, ok := os.LookupEnv(envName)
+	if !ok {
+		v = envDefault
+	}
+	return v
+}
+
+var urlMicroLogin = getEnvDefault("URL_MICRO_LOGIN", "http://localhost:5000")
+var urlMicroHistorico = getEnvDefault("URL_MICRO_HISTORICO", "http://localhost:5001")
