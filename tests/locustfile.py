cb6e5962e7be77478db3bd35c9e84f709b51c764
Leinadium <daniel.sch.guima@gmail.com>
2023-12-06 07:50:40 -0300

    chore: Movendo testes para /tests

--- tests/locustfile.py
+++ tests/locustfile.py
@@ -1,15 +1,15 @@
-from os import getenv
 from random import sample, randint
 from locust import HttpUser, task, between
 
 escolhas: list = [
-    {"disciplina":"INF1950","turma":"3WF"},
-    {"disciplina":"ENG1133","turma":"3VB"},
-    {"disciplina":"MQI2957","turma":"32A"},
-    {"disciplina":"ENG4402","turma":"3VA"},
-    {"disciplina":"ENG1443","turma":"3VA"},
-    {"disciplina":"ENG1438","turma":"3VA"}
+    {"disciplina": "INF1950", "turma": "3WF"},
+    {"disciplina": "ENG1133", "turma": "3VB"},
+    {"disciplina": "MQI2957", "turma": "32A"},
+    {"disciplina": "ENG4402", "turma": "3VA"},
+    {"disciplina": "ENG1443", "turma": "3VA"},
+    {"disciplina": "ENG1438", "turma": "3VA"}
 ]
+
 
 def recomendacao_aleatoria():
     return sample(escolhas, randint(0, len(escolhas) - 1))
@@ -25,7 +25,6 @@ class Usuario(HttpUser):
         self.client.post(f"/login?usuario={usuario}&senha={senha}")
 
         return super().on_start()
-    
 
     @task
     def recomendacao(self):
